5e42ce9cc72ee69862aceabb403a7334f6bd41a7
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-10-24 22:11:04 +0300

    fix issue in the port of the db

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,7 +6,7 @@ config();
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  port: parseInt(process.env.DB_PORT, 10) || 5433,
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || '1517',
   database: process.env.DB_NAME || 'fintech_db',
