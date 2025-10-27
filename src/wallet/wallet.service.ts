869341fb742a356d103e8b9ad8097958ad39ba03
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-10-27 03:22:45 +0300

    feat(): add function to the wallet service

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DataSource, Repository } from 'typeorm';
 import { Wallet } from './entities/wallet.entity';
@@ -74,5 +74,87 @@ export class WalletService {
         } finally {
             await queryRunner.release();
         }
+    }
+
+    async withdraw(userId: string, amount: number) {
+        const queryRunner = this.dataSource.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+
+        try {
+            const wallet = await queryRunner.manager.findOne(Wallet, { where: { userId }, lock: { mode: "pessimistic_write" } });
+            if (!wallet) {
+                throw new NotFoundException('Wallet not found');
+            }
+
+            const balanceBefore = Number(wallet.balance);
+            if (balanceBefore < amount) {
+                throw new BadRequestException('Insufficient balance');
+            }
+
+            const balanceAfter = balanceBefore - amount;
+            wallet.balance = balanceAfter;
+            await queryRunner.manager.save(wallet);
+
+            const transaction = queryRunner.manager.create(WalletTransaction, {
+                walletId: wallet.id,
+                type: TransactionType.WITHDRAW,
+                amount,
+                balanceBefore,
+                balanceAfter,
+                metadata: {
+                    description: 'Wallet withdraw'
+                }
+            });
+
+
+            await queryRunner.manager.save(transaction);
+            await queryRunner.commitTransaction();
+            return {
+                message: 'Withdraw successful',
+                balance: balanceAfter,
+                transaction: {
+                    id: transaction.id,
+                    type: transaction.type,
+                    amount: transaction.amount,
+                    createdAt: transaction.createdAt,
+                },
+            };
+
+        } catch (error) {
+            if (queryRunner.isTransactionActive) {
+                await queryRunner.rollbackTransaction();
+            }
+            throw error;
+        } finally {
+            await queryRunner.release();
+        }
+
+    }
+
+    async getTransactions(userId: string) {
+        const wallet = await this.walletRepository.findOne({
+            where: { userId },
+        });
+
+        if (!wallet) {
+            throw new NotFoundException('Wallet not found');
+        }
+
+        const transactions = await this.walletTransactionRepositroy.find({
+            where: { walletId: wallet.id },
+            order: { createdAt: 'DESC' },
+        });
+
+        return transactions.map((tx) => ({
+            id: tx.id,
+            type: tx.type,
+            amount: Number(tx.amount),
+            balanceBefore: Number(tx.balanceBefore),
+            balanceAfter: Number(tx.balanceAfter),
+            referenceId: tx.referenceId,
+            metadata: tx.metadata,
+            createdAt: tx.createdAt,
+        }));
     }
 }
