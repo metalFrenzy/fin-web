134f86ac00d6042a7285f95d2fa70b702ab1d89c
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-10-27 03:42:22 +0300

    feat(): add import and exports

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Wallet } from './entities/wallet.entity';
+import { WalletTransaction } from './entities/wallet-transactions.entity';
+import { WalletController } from './wallet.controller';
+import { WalletService } from './wallet.service';
 
-@Module({})
-export class WalletModule {}
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([Wallet, WalletTransaction])
+    ],
+    controllers: [WalletController],
+    providers: [WalletService],
+    exports: [WalletService],
+})
+export class WalletModule {
+
+}
