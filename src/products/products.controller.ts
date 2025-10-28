a9311210a12c72ce9d01db7a3eaeace30c393cdf
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-10-28 23:39:03 +0300

    feat(): add product controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,65 @@
-import { Controller } from '@nestjs/common';
+// src/products/products.controller.ts
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    UseGuards,
+    HttpCode,
+    HttpStatus,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/common/decorators/current-user.decorators';
+import { Roles } from 'src/common/decorators/roles.decorators';
+import { UserRole } from 'src/common/enum/user-role.enum';
+import { RolesGuard } from 'src/common/guards/role.guard';
+import { CreateProductDto, UpdateProductDto } from './dto/product.dto';
+
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+    constructor(private readonly productsService: ProductsService) { }
+
+    @Post()
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(UserRole.MERCHANT)
+    @HttpCode(HttpStatus.CREATED)
+    async create(
+        @CurrentUser() user: any,
+        @Body() createProductDto: CreateProductDto,
+    ) {
+        return this.productsService.create(user.id, createProductDto);
+    }
+
+    @Get()
+    async findAll() {
+        return this.productsService.findAll();
+    }
+
+    @Get('my-products')
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(UserRole.MERCHANT)
+    async getMyProducts(@CurrentUser() user: any) {
+        return this.productsService.findByMerchant(user.id);
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+        return this.productsService.findById(id);
+    }
+
+
+    @Patch(':id')
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(UserRole.MERCHANT)
+    async update(
+        @Param('id') id: string,
+        @CurrentUser() user: any,
+        @Body() updateProductDto: UpdateProductDto,
+    ) {
+        return this.productsService.updateProduct(id, updateProductDto, user.id,)
+    }
+}
