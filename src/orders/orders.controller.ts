a977cdfa6ba64d97283248c12db1b38d4d70822a
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-10-30 19:33:29 +0300

    fea(): add the endpooints

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { OrdersService } from './orders.service';
+import { CurrentUser } from 'src/common/decorators/current-user.decorators';
+import { CreateOrderDto } from './dto/order.dto';
+import { Roles } from 'src/common/decorators/roles.decorators';
+import { RolesGuard } from 'src/common/guards/role.guard';
+import { UserRole } from 'src/common/enum/user-role.enum';
 
 @Controller('orders')
-export class OrdersController {}
+@UseGuards(JwtAuthGuard)
+export class OrdersController {
+    constructor(private readonly orderService: OrdersService) { }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    async createOrder(@CurrentUser() user: any, @Body() orderDto: CreateOrderDto) {
+        return this.orderService.createOrder(user.id, orderDto);
+    }
+
+    @Get()
+    async getOrders(@CurrentUser() user: any) {
+        return this.orderService.getUserOrders(user.id);
+    }
+
+    @Get('sales')
+    @UseGuards(RolesGuard)
+    @Roles(UserRole.MERCHANT)
+    async getMerchantOrders(@CurrentUser() user: any) {
+        return this.orderService.getMerchantOrders(user.id)
+    }
+
+    @Get(':id')
+    async getOrderById(@CurrentUser() user: any, @Param('id') id: string) {
+        return this.orderService.getOrderById(user.id, id);
+    }
+}
