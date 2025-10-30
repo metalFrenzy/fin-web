ede2077aac18cc2b78e522bef17bc6b4368d9b53
Ahmed El Sisi <Ahmed.0881100@gmail.com>
2025-10-30 19:33:46 +0300

    feat(): add typeorm module in the order module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entites/order.entity';
+import { Wallet } from 'src/wallet/entities/wallet.entity';
+import { WalletTransaction } from 'src/wallet/entities/wallet-transactions.entity';
+import { Product } from 'src/products/entites/product.entity';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService]
+  providers: [OrdersService],
+  imports: [
+    TypeOrmModule.forFeature([Order, Wallet, WalletTransaction, Product])
+  ], exports: [OrdersService],
 })
-export class OrdersModule {}
+export class OrdersModule { }
